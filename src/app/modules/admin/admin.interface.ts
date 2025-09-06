4ac0bb3e103dd16c3eb8b43c54670bbc7c503293
jobair-talha <talhajobair10@gmail.com>
2025-09-06 22:10:35 +0600

    Refactor IAdmin type to simplify name structure and remove unused fields

--- src/app/modules/admin/admin.interface.ts
+++ src/app/modules/admin/admin.interface.ts
@@ -1,23 +1,15 @@
 import { Model, Types } from 'mongoose';
 import { IManagementDepartment } from '../managementDepartment/managementDepartment.inerface';
 
-export type UserName = {
-  firstName: string;
-  lastName: string;
-  middleName: string;
-};
+
 
 export type IAdmin = {
   id: string;
-  name: UserName;
+  name: string;
   profileImage: string;
-  dateOfBirth?: string;
   email: string;
-  contactNo: string;
-  emergencyContactNo: string;
-  gender?: 'male' | 'female';
-  permanentAddress?: string;
-  presentAddress?: string;
+  mobile: string;
+  address?: string;
   bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
   managementDepartment: Types.ObjectId | IManagementDepartment;
   designation: string;
