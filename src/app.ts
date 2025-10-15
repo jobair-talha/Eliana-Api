1b693e537bb16accbcb21b0fe06ff3bd27f46529
jobair-talha <talhajobair10@gmail.com>
2025-10-16 03:16:36 +0600

    Increase JSON payload size limit to 5mb in app configuration

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(cookieParser());
 
 //parser
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 // serving static files
